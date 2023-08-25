907653030da0ff6a8721d9c1c97bceb2d56a0a6e
arralia <aly.tan@outlook.com>
2023-08-25 09:16:04 -0700

    3:40 into tutorial video

--- cargame.cpp
+++ cargame.cpp
@@ -35,7 +35,104 @@ void setcursor(bool visible, DWORD size)
     if(size == 0) size = 20;
 
     CONSOLE_CURSOR_INFO lpCursor;
-    lpCursor.bVsisible = visible;
+    lpCursor.bVisible = visible;
     lpCursor.dwSize = size;
     SetConsoleCursorInfo(console, &lpCursor);
+}
+
+void drawBorder()
+{
+    for(int i=0;i<SCREEN_HEIGHT;++i)
+    {
+        for(int j=0;j<17;++j)
+        {
+            gotoxy(j,i);
+            cout<< "+";
+            gotoxy(WIN_WIDTH-j,i);
+            cout<<"+";
+        }
+    }
+    for(int i=0;i<SCREEN_HEIGHT;++i)
+    {
+        gotoxy(SCREEN_WIDTH,i);
+        cout<<"+";
+    }
+}
+
+void genEnemy(int ind)
+{
+    enemyX[ind] = 17+rand()%(33);
+}
+
+void drawEnemy(int ind)
+{
+    if(enemyFlag[ind] == true)
+    {
+        gotoxy(enemyX[ind],enemyY[ind]); cout<<"****";
+        gotoxy(enemyX[ind],enemyY[ind]+1); cout<<" ** ";
+        gotoxy(enemyX[ind],enemyY[ind]+2); cout<<"****";
+        gotoxy(enemyX[ind],enemyY[ind]+3); cout<<" ** ";
+    }
+}
+
+void eraseEnemy(int ind)
+{
+    if(enemyFlag[ind] == true)
+    {
+        gotoxy(enemyX[ind],enemyY[ind]); cout<<"    ";
+        gotoxy(enemyX[ind],enemyY[ind]+1); cout<<"    ";
+        gotoxy(enemyX[ind],enemyY[ind]+2); cout<<"    ";
+        gotoxy(enemyX[ind],enemyY[ind]+3); cout<<"    ";
+    }
+}
+
+void resetEnemy(int ind)
+{
+    eraseEnemy(ind);
+    enemyY[ind] = 1;
+    genEnemy(ind);
+}
+
+void drawCar()
+{
+    for(int i=0;i<4;++i)
+    {
+        for(int j=0;j<4;++j)
+        {
+            gotoxy(j+carPos,i+22);
+            cout<<car[i][j];
+        }
+    }
+}
+
+void eraseCar()
+{
+    for(int i=0;i<4;++i)
+    {
+        for(int j=0;j<4;++j)
+        {
+            gotoxy(j+carPos,i+22);
+            cout<< " ";
+        }
+    }
+}
+
+int collision()
+{
+    if(enemyY[0]+4 >= 23)
+    {
+        if(enemyX[0]+4 - carPos >= 0 && enemyX[0]+4 - carPos < 9) return 1;
+    }
+    return 0;
+}
+
+void gameover()
+{
+    system("cls");
+    cout<<endl;
+    cout<<"\t\t-----------------------------------"<<endl;
+    cout<<"\t\t-------------GAME OVER-------------"<<endl;
+    cout<<"\t\t-----------------------------------"<<endl;
+    cout<<"\t\tPress any key to return to the menu"<<endl;
+    getch();
 }
