66610d2d56e4172d182bda100807219183f3a9bf
arralia <aly.tan@outlook.com>
2023-09-08 22:04:24 -0700

    changing formatting

--- src/cargame.cpp
+++ src/cargame.cpp
@@ -175,11 +175,11 @@ void play()
     genEnemy(0);
     genEnemy(1);
 
-    gotoxy(WIN_WIDTH+7, 2); cout<<"Alyssia's Car Game!";
-    gotoxy(WIN_WIDTH+6, 4); cout<<"------------------";
-    gotoxy(WIN_WIDTH+6, 6); cout<<"------------------";
-    gotoxy(WIN_WIDTH+7, 12); cout<<"Control";
-    gotoxy(WIN_WIDTH+7, 13); cout<<"-----------";
+    gotoxy(WIN_WIDTH+5, 2); cout<<"Alyssia's Car Game!";
+    gotoxy(WIN_WIDTH+5, 4); cout<<"------------------";
+    gotoxy(WIN_WIDTH+5, 6); cout<<"------------------";
+    gotoxy(WIN_WIDTH+6, 12); cout<<"Control";
+    gotoxy(WIN_WIDTH+6, 13); cout<<"-----------";
     gotoxy(WIN_WIDTH+2, 14); cout<<"A Key - Left";
     gotoxy(WIN_WIDTH+2, 15); cout<<"D Key - Right";
 
